c58c301fc8ad1f0f49d84eba7641c6cafd7ae502
butakero <viltetomas2003@gmail.com>
2025-01-30 17:34:30 -0300

    test: Temporarily skip JiraService integration test

--- internal/infrastructure/tickets/jira/jira_service_integration_test.go
+++ internal/infrastructure/tickets/jira/jira_service_integration_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestJiraService_GetTicketInfo_Integration(t *testing.T) {
-	//t.Skip("skipping integration test")
+	t.Skip("skipping integration test")
 	// Arrange
 	client := &http.Client{}
 	service := &JiraService{
